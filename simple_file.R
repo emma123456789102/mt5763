5b03e5d9ed138d556658db3ce157cca72d1153cd
Emma Davidson <davidsone381@gmail.com>
2025-10-16 10:33:55 +0100

    writing the final tutorial for intro for data analysis

--- simple_file.R
+++ simple_file.R
@@ -1,1 +1,9 @@
-## working on github
+## Finial tutorial - for intro data analysis
+
+load("C:/Users/Emma Davidson/Downloads/brexitdf.Rdata")
+dim(brexitdf)
+head(brexitdf)
+simple.lm<-lm(leave ~ electorate, data = brexitdf)
+simple.lm
+plot(brexitdf$electorate,bexitdf$leave,xlab="who wanted brexit", ylab="who did want")
+anova(simple.lm)
